33dcd3dcfbc67aa118b87c3b0602ce7105eb9e02
Kayode-Olumo <46356143+Kayode-Olumo@users.noreply.github.com>
2023-06-24 13:35:09 +0100

    iniitial commit

---
+++ slack-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCegmjX1dHxzWkhjoobhuVJcQlQnCsSCjU",
+    authDomain: "slack-clone-aa74c.firebaseapp.com",
+    projectId: "slack-clone-aa74c",
+    storageBucket: "slack-clone-aa74c.appspot.com",
+    messagingSenderId: "567663736621",
+    appId: "1:567663736621:web:761b5e0914119c277a85a4"
+};
